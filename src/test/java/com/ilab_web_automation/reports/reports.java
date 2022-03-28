ecbb7cd00960d2f9759c7f9cb3ae2eb00d5d2190
Matome.Masiye <Matome.Masiye@ilabqa.com>
2022-03-28 21:38:59 +0200

    Added code that captures a screenshot upon validation

---
+++ src/test/java/com/ilab_web_automation/reports/reports.java
@@ -0,0 +1,37 @@
+package com.ilab_web_automation.reports;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Base64;
+
+public class reports {
+
+    public ExtentSparkReporter spark;
+    public ExtentReports extent;
+
+    public ExtentReports initializeReports(String reportFileName){
+        spark = new ExtentSparkReporter(reportFileName);
+        extent = new ExtentReports();
+        extent.attachReporter(spark);
+
+        return extent;
+
+    }
+
+    public String captureScreenshot(WebDriver driver) throws IOException {
+
+        File sFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        byte [] fileContent = FileUtils.readFileToByteArray(new File(String.valueOf(sFile)));
+        String encodedString = Base64.getEncoder().encodeToString(fileContent);
+
+        return encodedString;
+    }
+
+}
---
+++ src/test/java/com/ilab_web_automation/validations/validations.java
@@ -0,0 +1,50 @@
+package com.ilab_web_automation.validations;
+
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.ilab_web_automation.reports.reports;
+import com.ilab_web_automation.webPageObjects.iLabApplicationForm;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.testng.Assert;
+
+import java.time.Duration;
+
+
+public class validations {
+
+    reports repo = new reports();
+
+    public void validateApplication(WebDriver driver, ExtentTest node){
+
+        iLabApplicationForm appForm = new iLabApplicationForm(driver);
+
+        Wait<WebDriver> wait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(5))
+                .pollingEvery(Duration.ofMillis(1000))
+                .ignoring(WebDriverException.class);
+        wait.until(ExpectedConditions.visibilityOf(appForm.errorMessage));
+
+        try{
+
+            String filename = repo.captureScreenshot(driver);
+
+            if(appForm.errorMessage.isDisplayed()){
+
+                node.pass("Error message displayed",MediaEntityBuilder.createScreenCaptureFromBase64String(filename).build());
+            }else{
+                node.fail("Error message not displayed",MediaEntityBuilder.createScreenCaptureFromBase64String(filename).build());
+                Assert.fail("Error message not displayed");
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
