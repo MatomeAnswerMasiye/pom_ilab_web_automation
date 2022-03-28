7bc5a2c5d5cc3d908c8f3b0fa733e9a380850fe2
Matome.Masiye <Matome.Masiye@ilabqa.com>
2022-03-28 13:48:08 +0200

    Added the test runner,webUtilities and webActions classes

--- src/test/java/com/ilab_web_automation/webUtilities/webUtilities.java
+++ src/test/java/com/ilab_web_automation/webUtilities/webUtilities.java
@@ -3,21 +3,27 @@ package com.ilab_web_automation.webUtilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chromium.ChromiumDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class webUtilities {
 
     WebDriver driver;
 
+    //Returns a web driver when invoked
     public WebDriver getWebDriver(){
         return driver;
     }
 
+
+    //Sets the web driver
     public void setWebDriver(WebDriver theWebDriver){
         this.driver = theWebDriver;
     }
 
+    //Takes the browser as a string input and returns when invoked
     public WebDriver initializeWebDriver(String sBrowser){
         switch (sBrowser.toUpperCase()){
             case "CHROME":
@@ -30,7 +36,7 @@ public class webUtilities {
                 break;
 
             case "IE":
-                WebDriverManager.chromiumdriver().setup();
+                WebDriverManager.edgedriver().setup();
                 driver = new EdgeDriver();
                 break;
 
@@ -41,6 +47,7 @@ public class webUtilities {
     }
 
 
+    //Takes the web url as an input and navigates to the website/application
     public void navigateToUrl(String url){
 
         driver.get(url);
